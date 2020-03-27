8d1490afbf8f0796d50ebdd2ebce09727ca2a90d
vvv <vvv@v33bz.abc.org>
2020-03-27 14:07:09 +0100

    veraenderte namensgebung html-dateien

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from selenium import webdriver
 import time
 import urllib.request
 import re
+import os
+import datetime
 
 
 # Scraper für Google Assistant Directory
@@ -40,12 +42,14 @@ def open_from_directory(filename):
         return html_string
 
 
-def create_filename(string, type):
+def create_filename(string, *oberkategorie):
     filename = re.sub("[^A-Za-z0-9]+", "", string)
     filename = filename.lower()
-    if type == "category":
+    if not oberkategorie:
         filename += "-category.html"
     else:
+        filename += "-"
+        filename += oberkategorie[0]
         filename += "-subcategory.html"
     return filename
 
@@ -62,6 +66,20 @@ def make_url(urlanhang):
     url += urlanhang
     return url
 
+# noch nicht fertig
+# def scrape_oberkategorien():
+    # neuen Ordner mit Timestamp erstellen
+    # dt_date = datetime.datetime.now()
+    # zeitstempel = dt_date.strftime(%d-%m-%Y-%I-%M)
+    # path = "/gad_scrape_"
+    # path += zeitstempel
+    # os.mkdir(path)
+
+# noch nicht fertig
+# def scrape_unterkategorien():
+    # dir = 'path/to/Pics2'
+    # if not os.path.isdir(dir): os.makedirs(dir)
+
 
 # Scrape auf Startseite starten
 save_html("https://assistant.google.com/explore", "start.html")
@@ -70,8 +88,11 @@ soup_start = BeautifulSoup(start, "html.parser")
 
 for a in soup_start.find_all("a", "hSRGPd", href=True, jslog=True)[1:19]:
     label = a['aria-label']
+    global label_oberkategorie
+    label_oberkategorie = re.sub("[^A-Za-z0-9]+", "", label)
+    label_oberkategorie = label_oberkategorie.lower()
     url = make_url(a['href'])
-    filename = create_filename(label, "category")
+    filename = create_filename(label)
 
     # htmls für Oberkategorien speichern
     save_html(url, filename)
@@ -84,6 +105,17 @@ for a in soup_start.find_all("a", "hSRGPd", href=True, jslog=True)[1:19]:
     # htmls nach Unterkategorien durchsuchen und die entsprechenden Seiten speichern
     for a in soup_oberkategorie.find_all("div", "dLQiFb"):
         label_unterkategorie = a['data-title']
-        filename_unterkategorie = create_filename(label_unterkategorie, "subcategory")
+        filename_unterkategorie = create_filename(label_unterkategorie, label_oberkategorie)
         url = make_url(a['data-link'])
         save_html(url, filename_unterkategorie)
+
+
+        # htmls nach Actions durchsuchen und Daten ablegen
+        sourcecode_unterkategorie = open_from_directory(filename_unterkategorie)
+        soup_unterkategorie = BeautifulSoup(sourcecode_unterkategorie, "html.parser")
+
+        for a in soup_unterkategorie.find_all("div", "FdWgBb nhLye"):
+        action_name = a.contents
+        print(action_name)
+
+
